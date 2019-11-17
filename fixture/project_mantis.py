0841c27aa6844b96606dee2f30b3a46feafccec6
DariaMagarshak <55354330+DariaMagarshak@users.noreply.github.com>
2019-11-18 00:55:00 +0300

    Тест добавить проект2

--- fixture/project_mantis.py
+++ fixture/project_mantis.py
@@ -36,11 +36,19 @@ class ProjectMantisHelper:
 
             self.project_cache = []
             #wd.find_element_by_xpath("//tr[class='row-%s']" % index)
-            #for element in wd.find_elements_by_xpath("//tr[class='row']"):
-            for element in wd.find_elements_by_xpath("//tr[contains(@class='row')]"):
-                cells = element.find_elements_by_tag_name("td")
-                name_text = cells[0].text
-                description_text = cells[4].text
-                self.project_cache.append(ProjectMantis(name=name_text, description=description_text))
+            #for element in wd.find_elements_by_xpath("//tr[class='row']")("//a[contains(@href='manage_proj_edit_page.php?project_id')]"):
 
-        return list(self.project_cache)
+            #table = wd.find_elements_by_xpath("//tr[class='form-title']")
+            #table = wd.find_elements_by_css_selector("td.form-title, colspan='5'")
+            table = wd.find_elements_by_xpath("//table[@class='width100']")
+            del table[0]
+            for t in table:
+                rows = wd.find_elements_by_xpath("//tr[contains(@class, 'row')]")
+                del rows[0]
+                for element in rows:
+                    cells = element.find_elements_by_tag_name("td")
+                    name_text = cells[0].text
+                    description_text = cells[4].text
+                    self.project_cache.append(ProjectMantis(name=name_text, description=description_text))
+        pr_cache = list(self.project_cache)[:-1]
+        return pr_cache
