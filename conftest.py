a32c4d8cf9ecc6b1118aa435a43b53575c461970
DariaMagarshak <55354330+DariaMagarshak@users.noreply.github.com>
2019-11-17 16:42:25 +0300

    Тест "Логин"

---
+++ conftest.py
@@ -0,0 +1,50 @@
+# задание 17
+import pytest
+from fixture.application import Application
+import json
+import os.path
+
+
+
+
+fixture = None
+target = None
+def load_config(file):
+    global target
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file) as f:
+           target = json.load(f)
+    return target
+
+
+
+@pytest.fixture
+def app(request):
+    global fixture
+    browser = request.config.getoption("--browser")
+    web_config = load_config(request.config.getoption("--target"))['web']
+    # получаем доступ к сохраненному параметру через объект request
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=web_config['baseUrl'])
+
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.session.ensure_logout()
+        fixture.destroy()
+    request.addfinalizer(fin)
+    return fixture
+
+def pytest_addoption(parser):
+#параметр browser, действие "сохранить параметр", дефолтное значение
+    #parser.addoption("--password", action="store", default = "secret")
+    parser.addoption("--browser", action="store", default = "firefox")
+    parser.addoption("--target", action="store", default = "target.json")
+
+
+
+
